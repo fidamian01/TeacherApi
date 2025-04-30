cab90dd053667a0a2fb4cb31635cb5f2e92f3328
fidamian01 <fakunleinioluwa@gmail.com>
2025-04-30 10:49:30 +0100

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,16 +1,23 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Options;
 using TeacherApi.Data;
+using TeacherApi.Services;
+using TeacherApi.Services.Interfaces;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddDbContext<SchoolDbContext>(options => options.UseSqlite("Data Source = school.db"));
+builder.Services.AddDbContext<SchoolDbContext>(options =>
+    options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddScoped<ITeacherService, TeacherService>();
+builder.Services.AddScoped<IStudentService, StudentService>();
+builder.Services.AddScoped<IClassroomService, ClassroomService>();
+
 
 var app = builder.Build();
 
